f977fd03c3fe72871703521d811fe384b36e34a5
Louis Stevenson <louie.j.stevenson@gmail.com>
2023-09-04 23:42:36 -0500

    Completed first section of ng-book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // DECLARATIONS MEAN DEFINED WITHIN THIS APP, WHICH ARE ALL IN SUBFOLDERS OF THE MAIN APP.MODULE/APP.COMPONENT
+    AppComponent,
+    HelloWorldComponent,
+    UserItemComponent,
+    UserListComponent
   ],
-  imports: [
+  imports: [ // THESE ARE PARENT-LEVEL IMPORTS, WHICH IS SIGNIFIED BY THE @ SYMBOL 
     BrowserModule
   ],
   providers: [],
